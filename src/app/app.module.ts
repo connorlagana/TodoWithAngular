855b6ad23abac825c98d83e632d238a1fb1c6dda
Connor Lagana <connor.lagana@gmail.com>
2019-12-22 21:34:08 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
