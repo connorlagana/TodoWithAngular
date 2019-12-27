6e642128855057c06a271f742511a88edb2a236d
Connor Lagana <connor.lagana@gmail.com>
2019-12-27 00:12:50 -0500

    addToggle funvtion

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -26,4 +26,11 @@ export class TodoItemComponent implements OnInit {
     return classes
   }
 
+  onToggle(todo) {
+    console.log('toggle')
+  }
+  onDelete(todo) {
+    console.log('deleted')
+  }
+
 }
